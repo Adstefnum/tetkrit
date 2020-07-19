15c0d05b0e7aac7dfb58af17f5d18b72d703aa1a
Stephen Adesina <bauerstevenson@gmail.com>
2020-07-19 20:38:20 +0100

    lambda functions

--- lambda.py
+++ lambda.py
@@ -1,5 +1,68 @@
-import hashlib
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import ast
 
-enc_word = word.encode('utf-8')
-encrypted = hashlib.md5(enc_word.strip()).hexdigest()
+
+def lambda_handler(event):
+	pass
+
+def rsa(word_or_hash, mode):
+
+	random_generator = Random.new().read
+	key = RSA.generate(2048, random_generator) #generate pub and priv key
+	publickey = key.publickey() # pub key export for exchange
+
+	if mode == encrypt:
+		encrypted = publickey.encrypt(word_or_hash, 32)
+		return encrypted
+
+	else:
+		decrypted = key.decrypt(ast.literal_eval(str(word_or_hash)))
+		return decrypted
+
+def tripleDes(word_or_hash, mode):
+
+	if mode == encrypt:
+		encrypted = 
+		return encrypted
+
+	else:
+		decrypted = 
+		return decrypted
+
+def tripleDes(word_or_hash, mode):
+
+	if mode == encrypt:
+		encrypted = 
+		return encrypted
+
+	else:
+		decrypted = 
+		return decrypted
+
+def tripleDes(word_or_hash, mode):
+
+	if mode == encrypt:
+		encrypted = 
+		return encrypted
+
+	else:
+		decrypted = 
+		return decrypted
+
+def tripleDes(word_or_hash, mode):
+
+	if mode == encrypt:
+		encrypted = 
+		return encrypted
+
+	else:
+		decrypted = 
+		return decrypted
+
+
+
+
+
 
