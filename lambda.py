d1e66c924e6dc55dc0a92f306f338608656545b5
Stephen Adesina <bauerstevenson@gmail.com>
2020-07-19 21:43:09 +0100

    lambda

--- lambda.py
+++ lambda.py
@@ -1,7 +1,9 @@
 import Crypto
+from Crypto.Cipher import *
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import ast
+from struct import pack
 
 
 def lambda_handler(event):
@@ -22,43 +24,66 @@ def rsa(word_or_hash, mode):
 		return decrypted
 
 def tripleDes(word_or_hash, mode):
+	key = 'Sixteen byte key'
+	iv = Random.new().read(DES3.block_size) #DES3.block_size==8
 
 	if mode == encrypt:
-		encrypted = 
+		cipher_encrypt = DES3.new(key, DES3.MODE_OFB, iv)
+		encrypted = cipher_encrypt.encrypt(word_or_hash)
 		return encrypted
 
 	else:
-		decrypted = 
+		cipher_decrypt = DES3.new(key, DES3.MODE_OFB, iv) 
+		decrypted = cipher_decrypt.decrypt(encrypted_text)
 		return decrypted
 
-def tripleDes(word_or_hash, mode):
+def aes(word_or_hash, mode):
+	key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+	iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
 
 	if mode == encrypt:
-		encrypted = 
+		aes = AES.new(key, AES.MODE_CBC, iv)
+		encrypted = aes.encrypt(data)
 		return encrypted
 
 	else:
-		decrypted = 
+		aes = AES.new(key, AES.MODE_CBC, iv)
+		decrypted = adec.decrypt(encd)
 		return decrypted
 
-def tripleDes(word_or_hash, mode):
+'''def blowFish(word_or_hash, mode):
+
+	bs = Blowfish.block_size
+ 	key = b'An arbitrarily long key'
+ 	iv = Random.new().read(bs)
+ 	cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+ 	plen = bs - divmod(len(word_or_hash),bs)[1]
+ 	padding = [plen]*plen
+ 	padding = pack('b'*plen, *padding)
 
 	if mode == encrypt:
-		encrypted = 
+		encrypted = iv + cipher.encrypt(word_or_hash + padding)
 		return encrypted
 
 	else:
-		decrypted = 
-		return decrypted
+		iv = word_or_hash[:bs]
+		word_or_hash = word_or_hash[bs:]
+		decrypted = cipher.decrypt(word_or_hash)
+		last_byte = decrypted[-1]
+		decrypted = decrypted[:- (last_byte if type(last_byte) is int else last_byte)]
+		return repr(decrypted)'''
 
-def tripleDes(word_or_hash, mode):
+
+def blowFish(word_or_hash, mode):
+	key = b"bbgjbdrguhdghsebgiufvffdcjvfkjfcfghfkdcjfkdjfkhdgvkhjdgfhdcgjfkhdhjfkdgfkdgjfkhdfkdgjdhjjdchjhdjjhdjhjjhfdjjdjddgdjgdgxgdcvjgdvvgdchxdcvhgdjcghdccxdjvdgvgdxcvdhgjxvhdgjcghcdhcdcdchdchfchf"
+	cipher = Blowfish.new(key)
 
 	if mode == encrypt:
-		encrypted = 
+		encrypted = cipher.encrypt(word_or_hash)
 		return encrypted
 
 	else:
-		decrypted = 
+		decrypted = cipher.decrypt(word_or_hash)
 		return decrypted
 
 
